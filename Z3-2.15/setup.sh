a4404339f0a669f12b944e6642339e981516b844
Finn Haedicke <finn@informatik.uni-bremen.de>
2011-03-03 16:22:59 +0100

    added package for SMT solver Z3 in versions 2.15, 2.18 an 2.19

---
+++ Z3-2.15/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+if [ -z "$build" ] ; then 
+  echo '$build is undefined'
+  exit 1
+fi
+if [ -z "$package_dir" ] ; then 
+  echo '$build is undefined'
+  exit 1
+fi
+
+package=Z3
+version=2.15
+source=z3-x64-$version.tar.gz
+build_dir=$build/z3-x64-$version
+url=http://research.microsoft.com/projects/z3/$source
+
+download_unpack() {
+  cd $build &&
+  [ -f $source ] || wget -O $source $url &&
+  tar -xf $source &&
+  rm -rf $build_dir &&
+  mv -f z3 $build_dir
+}
+
+
+pre_build() {
+  cp $package_dir/Z3Config.cmake $build_dir
+}
+
+build_install() {
+  if [ -z "$target" ] ; then 
+    echo '$target is undefined'
+    exit 1
+  fi
+  mkdir -p $target &&
+  cp -r $build_dir/* $target 
+}
