4cc24d106331b5613a573155c9a3aeff18396d4e
Heinz Riener <hriener@informatik.uni-bremen.de>
2012-10-15 11:08:00 +0200

    added boolector-1.5.118 package

---
+++ boolector-1.5.118/setup.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+
+if [ -z "$build" ] ; then 
+  echo '$build is undefined'
+  exit 1
+fi
+if [ -z "$package_dir" ] ; then 
+  echo '$package_dir is undefined'
+  exit 1
+fi
+
+
+package=boolector
+version=1.5.118
+extra_ver=6b56be4-121013
+source=$package-$version-$extra_ver.tar.gz
+build_dir=$build/$package-$version-$extra_ver
+url=http://fmv.jku.at/$package/$source
+
+dependencies="minisat-git"
+
+download_unpack() {
+  if [ "$duplicate" = "remove" ]; then
+    rm -rf $build_dir
+  fi
+  mkdir -p $(dirname $build_dir) &&
+  cd $(dirname $build_dir) &&
+  wget -c -O $source $url &&
+  tar -xf $source &&
+  cd $build_dir &&
+  patch -p1 < $package_dir/minisat_include_path.patch &&
+  patch -p1 < $package_dir/0003*.patch
+  patch -p1 < $package_dir/0014*.patch
+}
+
+
+pre_build() {
+  cd $build_dir &&
+  install_cmake_files
+}
+
+build_install() {
+  if [ -z "$target" ] ; then 
+    echo '$target is undefined'
+    exit 1
+  fi
+  cd $build_dir &&
+  cmake_build_install
+}
