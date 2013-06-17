42bb2e5a8dcdf8a51a4750dfadbe9b0fc7ec9e4c
Heinz Riener <hriener@informatik.uni-bremen.de>
2013-06-17 15:59:28 +0200

    added package for Z3-git version

---
+++ Z3-git/setup.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+if [ -z "$build" ] ; then 
+  echo '$build is undefined'
+  exit 1
+fi
+if [ -z "$package_dir" ] ; then 
+  echo '$build is undefined'
+  exit 1
+fi
+
+version=git
+branch=unstable
+package=Z3
+source=nosourcefile
+build_dir=$build/$package-$version
+url=https://git01.codeplex.com/z3 
+
+download() {
+  mkdir -p $cache/$package-$version &&
+  cd $cache/$package-$version &&
+  if [ -d .git ]; then
+    git pull
+  else
+    git clone $url .
+    git checkout $branch
+  fi
+}
+
+unpack() {
+  cp -R $cache/$package-$version $build_dir
+}
+  
+pre_build() {
+  true
+}
+ 
+build_install() {
+  if [ -z "$target" ] ; then
+    echo '$target is undefinde'
+    exit 1
+  fi
+  cd "$build_dir" &&
+  python scripts/mk_make.py --staticlib --prefix="$target" &&
+  cd build &&
+  make &&
+  make install &&
+  cp "$package_dir/Z3Config.cmake" "$target"
+}
