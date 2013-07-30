3de75cbca786a7433f97997add9c9fea01142405
Hoang M. Le <hle@informatik.uni-bremen.de>
2013-07-30 14:58:23 +0200

    added Z3-4.3.1 (master branch from Z3-git), download link used to avoid git cloning errors on e.g. Ubuntu 12.04

---
+++ Z3-4.3.1/setup.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+if [ -z "$build" ] ; then 
+  echo '$build is undefined'
+  exit 1
+fi
+if [ -z "$package_dir" ] ; then 
+  echo '$build is undefined'
+  exit 1
+fi
+
+version=4.3.1
+package=Z3
+source=z3-$version.zip
+build_dir=$build/$package-$version
+url='http://download-codeplex.sec.s-msft.com/Download/SourceControlFileDownload.ashx?ProjectName=z3&changeSetId=89c1785b73225a1b363c0e485f854613121b70a7' 
+
+download() {  
+  cd $cache &&
+  if [ ! -f $source ]; then
+    download_http $source $url
+  fi
+}
+
+unpack() {
+  mkdir -p $build_dir &&
+  unzip -o $source -d $build_dir
+}
+  
+pre_build() {
+  true
+}
+ 
+build_install() {
+  if [ -z "$target" ] ; then
+    echo '$target is undefinde'
+    exit 1
+  fi
+  cd "$build_dir" &&
+  autoconf &&
+  ./configure --prefix="$target" &&
+  python scripts/mk_make.py &&
+  cd build &&
+  sed -i '/site-packages/d' Makefile && 
+  make &&
+  make install &&
+  cp "$package_dir/Z3Config.cmake" "$target"
+}
+
