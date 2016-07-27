30899a0a285a3fcedbc504ba4429327d1b7f5a0f
Niklas Bruns <nbruns@uni-bremen.de>
2016-07-27 19:28:44 +0200

    add yices-2.4.2

---
+++ yices-2.4.2/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+version=2.4.2
+
+cmake_files_dir=$base_dir/yices-${version}
+
+source $base_dir/yices-${version}/shared.sh
---
+++ yices-2.4.2/shared.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ -z "$build" ] ; then 
+  echo '$build is undefined'
+  exit 1
+fi
+if [ -z "$package_dir" ] ; then 
+  echo '$package_dir is undefined'
+  exit 1
+fi
+
+package=yices
+version=2.4.2
+source=$package-$version-src.tar.gz
+build_dir=$build/$package-$version
+url="http://yices.csl.sri.com/cgi-bin/yices2-newnewdownload.cgi?file=$source&accept=I+Agree"
+
+unpack(){
+  cd $cache &&
+  tar -xf $source &&
+  cd 
+  rm -rf $build_dir &&
+  mv -f $cache/$package-$version $build_dir
+}
+
+pre_build() {
+  cd $build_dir &&
+  install_cmake_files $cmake_files_dir
+}
+
+build_install() {
+  if [ -z "$target" ] ; then 
+    echo '$target is undefined'
+    exit 1
+  fi
+  cd $build_dir &&
+  ./configure --prefix="$target" &&
+  make &&
+  make install &&
+  install_cmake_files $cmake_files_dir &&
+  cp YicesConfig.cmake "$target"
+}
+
+# vim: ts=2 sw=2 et
