dc865e01bb84715b40cf6b34493eee2828e1c4c6
Niklas Bruns <nbruns@uni-bremen.de>
2017-01-20 22:28:22 +0100

    add boolector 2.3.1

---
+++ boolector-2.3.1/shared.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+if [ -z "$build" ] ; then 
+  echo '$build is undefined'
+  exit 1
+fi
+if [ -z "$package_dir" ] ; then 
+  echo '$package_dir is undefined'
+  exit 1
+fi
+
+package=boolector
+version=2.3.1-with-lingeling-bbc
+source=$package-$version.tar.bz2
+build_dir=$build/$package-$version
+url=http://fmv.jku.at/$package/$source
+
+boolector_package=boolector-2.3.1-8d1d803-161217
+boolector_prefix=boolector-src
+
+unpack(){
+  cd $cache &&
+  tar -xf $source &&
+  rm -rf $build_dir &&
+  tar -xf $package-$version/archives/$boolector_package.tar.gz -C $package-$version/ && 
+  cd $package-$version &&
+  ln -sf $boolector_package ${boolector_prefix}  && 
+  mv -f $cache/$package-$version $build_dir
+  cd $build_dir/${boolector_prefix} &&
+  cd src &&
+  patch -p1 -i $patches_dir/minisat_include.patch &&
+  patch -p1 -i  $patches_dir/lingeling_include_path.patch &&
+  patch -p1 < $patches_dir/0014*.patch
+}
+
+pre_build() {
+  cd $build_dir &&
+  install_cmake_files $cmake_files_dir
+}
+
+build_install() {
+  if [ -z "$target" ] ; then 
+    echo '$target is undefined'
+    exit 1
+  fi
+  cd $build_dir &&
+  cmake_build_install
+}
+
+# vim: ts=2 sw=2 et
