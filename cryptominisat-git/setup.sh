45bc65827543f09c2ec2665258f0af6d753e46ef
Hoang M. Le <hle@informatik.uni-bremen.de>
2017-01-24 16:23:31 +0100

    remove boost dependency from cryptominisat-git

--- cryptominisat-git/setup.sh
+++ cryptominisat-git/setup.sh
@@ -15,13 +15,7 @@ source=$package-$version.tar.bz2
 build_dir=$build/${package}-${version}
 url="https://github.com/msoos/cryptominisat.git"
 
-if [ -z "$BOOST_ROOT" ]; then
-  dependencies="m4ri-20140914 $DEPS_BOOST"
-  boost_path="$root/$DEPS_BOOST"
-else
-  dependencies="m4ri-20140914"
-  boost_path="$BOOST_ROOT"
-fi
+dependencies="m4ri-20140914"
 
 download() {
   download_git "$build_dir" "$url" "master"
@@ -42,8 +36,9 @@ build_install() {
   fi
   # Note that the directory is packaged with an additional '4' in the name
   cd "$build_dir" &&
-  export CMAKE_PREFIX_PATH="$root/m4ri-20140914:$boost_path" &&
+  export CMAKE_PREFIX_PATH="$root/m4ri-20140914" &&
   cmake_build_install .. \
 	-DCMAKE_DISABLE_FIND_PACKAGE_PythonInterp=true \
-	-DCMAKE_DISABLE_FIND_PACKAGE_PythonLibs=true
+	-DCMAKE_DISABLE_FIND_PACKAGE_PythonLibs=true \
+	-DONLY_SIMPLE=ON
 }
